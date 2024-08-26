fa6b3c0987aa7e1b80a8cb0ac233e49830917705
Hinatatata <93185474+Hinatatata@users.noreply.github.com>
2024-08-26 18:49:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,35 +1,49 @@
+
 experiment_params = {
-    "val_ratio": 0.1,
-    "test_ratio": 0.1,
     "normalize_flag": True,
-    "model": "cru_net",
-    "device": 'cuda'
+    "model": "CResU_Net",
+    "device": 'cuda',
+    "operation_mode": "train_mode"
 }
 
 data_params = {
-    "weather_raw_dir": 'H:\\OSTIA',
-    "spatial_range": [[12,16], [115, 120]],
+    "weather_raw_dir": 'F:\\OSTIA',
+    "area_name": "NSCS",
+    "spatial_range": [[17, 22], [113, 120]],  # NSCS [17, 22], [113, 120], WSCS[8, 18], [109, 113], ESCS[12, 16],[115, 120]
     "weather_freq": 1,
-    "downsample_mode": "selective",
+    "downsample_mode": "selective",  # can be average or selective
     "check_files": False,
     "features": ['analysed_sst'],
     "target_dim": 0,
-    "rebuild": True
+    "rebuild": False,
+
+    "train_period": {
+            "start": "1995-01-01",
+            "end": "2010-12-31"
+    },
+    "val_period": {
+            "start": "2011-01-01",
+            "end": "2012-12-31"
+    },
+    "test_period": {
+            "start": "2013-01-01",
+            "end": "2013-12-31"
+    }
 }
 
 model_params = {
-
-    "cru_net": {
+    "CResU_Net": {
         "batch_gen": {
             "input_dim": 0,
             "output_dim": 0,
             "window_in_len": 10,
-            "window_out_len":10,
+            "window_out_len": 10,
             "batch_size": 1,
-            "shuffle": False,
+            "shuffle": True,
+            "seed": 1
         },
         "trainer": {
-            "num_epochs": 100,
+            "num_epochs": 1,
             "momentum": 0.7,
             "optimizer": "adam",
             "weight_decay": 0.0002,
@@ -43,5 +57,4 @@ model_params = {
             "out_channels": 10
         }
     },
-
 }
