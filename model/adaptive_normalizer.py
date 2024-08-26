e3f6c9d82693aa40645c1514451c3606439f77b6
Hinatatata <93185474+Hinatatata@users.noreply.github.com>
2024-08-26 18:51:27 +0800

    Update adaptive_normalizer.py

--- model/adaptive_normalizer.py
+++ model/adaptive_normalizer.py
@@ -2,7 +2,9 @@ import torch
 import numpy as np
 import numpy.ma as ma
 
+
 class AdaptiveNormalizer:
+
     def __init__(self, output_dim, seasonality=''):
         self.output_dim = output_dim
         self.seasonality = seasonality
@@ -10,25 +12,15 @@ class AdaptiveNormalizer:
 
     def norm(self, x):
 
-        """
-        Normalizes input tensor.
-
-        :param torch.tensor input_tensor: (T, M, N, D)
-        :return: normalized tensor
-        :rtype: torch.Tensor
-        """
-
         batch_size, seq_len, height, width, d_dim = x.shape
-
         x = np.maximum(x, 0)
-
-        tt=[]
+        tt = []
 
         for i in range(seq_len):
-            s = x[:,i,:,:,:]
+            s = x[:, i, :, :, :]
             mask = s == 0
             mx = ma.array(s, mask=mask)
-            mean=np.mean(mx)
+            mean = np.mean(mx)
             idx1 = np.where(s == 0)
             s[idx1] = mean
             tt.append(s)
@@ -50,16 +42,9 @@ class AdaptiveNormalizer:
             self.min_max.append((min_a, max_a))
 
         out = torch.stack(out, dim=-1)
-
         return out
 
     def inv_norm(self, x, device):
-        """
-        Take inverse normalization of the input tensor.
-
-        :param torch.tensor input_tensor: (b, t, d, m, n)
-        :return:
-        """
 
         x = x.permute(0, 1, 3, 4, 2)
         batch_size, seq_len, height, width, d_dim = x.shape
